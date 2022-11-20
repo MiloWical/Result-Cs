dc8e8b019292067d3212a0b9df50d0b4beaf899a
Milo Wical <milo.wical@gmail.com>
2022-11-20 08:40:30 -0500

    Back on the right track with Option.

--- src/ResultCs/Ok.cs
+++ src/ResultCs/Ok.cs
@@ -1,21 +1,21 @@
 namespace WicalWare.Components.ResultCs;
 
-public class Ok<T, E> : Result<T, E>
-{
-  private T _value { get; init; }
-  public ResultKind Kind => ResultKind.Ok;
-  public Option<E> ErrOption() => new None<E>();
+// public class Ok<T, E> : Result<T, E>
+// {
+//   private T _value { get; init; }
+//   public ResultKind Kind => ResultKind.Ok;
+//   public Option<E> ErrOption() => new None<E>();
 
-  // I can't completely replicate the Rust syntax because of the follow compiler error:
-  // error CS0542: 'Ok': member names cannot be the same as their enclosing type
-  public Option<T> OkOption() => new Some<T>(_value);
+//   // I can't completely replicate the Rust syntax because of the follow compiler error:
+//   // error CS0542: 'Ok': member names cannot be the same as their enclosing type
+//   public Option<T> OkOption() => new Some<T>(_value);
 
-  public Ok(T value)
-  {
-    ArgumentNullException.ThrowIfNull(value);
+//   public Ok(T value)
+//   {
+//     ArgumentNullException.ThrowIfNull(value);
 
-    _value = value;
-  }
+//     _value = value;
+//   }
 
-  public T Unwrap() => _value;
-}
+//   public T Unwrap() => _value;
+// }
--- src/ResultCs/UnwrapException.cs
+++ src/ResultCs/UnwrapException.cs
@@ -2,5 +2,13 @@ namespace WicalWare.Components.ResultCs;
 
 public class UnwrapException : Exception
 {
-  public override string Message => "Called 'Option.unwrap()' on a 'None' value";
+  private string _message = "Called 'Option.unwrap()' on a 'None' value";
+  public override string Message => _message;
+
+  internal UnwrapException() { }
+
+  internal UnwrapException(string errMessage)
+  {
+    _message = errMessage;
+  }
 }
