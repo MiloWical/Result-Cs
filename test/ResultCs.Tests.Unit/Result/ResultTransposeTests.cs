7b0051669634067430ab41061c752cf5693d740b
Milo Wical <milo.wical@gmail.com>
2023-05-30 15:09:55 -0400

    Completed Result.Transpose().

--- test/ResultCs.Tests.Unit/Result/ResultTransposeTests.cs
+++ test/ResultCs.Tests.Unit/Result/ResultTransposeTests.cs
@@ -7,6 +7,40 @@ public class ResultTransposeTests
   {
     var x = Result<Option<int>, string>.Ok(Option<int>.Some(5));
     var y = Option<Result<int, string>>.Some(Result<int, string>.Ok(5));
-    var transpose = x.Transpose();
+    var transpose = x.Transpose<int>();
+
+    Assert.True(y.Equals(transpose));
+  }
+
+  [Fact]
+  public void ResultTransposeOkNoneTest()
+  {
+    var x = Result<Option<int>, string>.Ok(Option<int>.None());
+    var y = Option<Result<int, string>>.None();
+    var transpose = x.Transpose<int>();
+
+    Assert.True(y.Equals(transpose));
+  }
+
+  [Fact]
+  public void ResultTransposeOkNonOptionTypeTest()
+  {
+    Assert.Throws<PanicException>(() => Result<int, string>.Ok(5).Transpose<int>());   
+  }
+
+  [Fact]
+  public void ResultTransposeOkUnassignableTypeTest()
+  {
+    Assert.Throws<PanicException>(() => Result<string, string>.Ok("5").Transpose<int>());   
+  }
+
+  [Fact]
+  public void ResultTransposeErrSomeTest()
+  {
+    var x = Result<Option<int>, string>.Err("Bad mojo");
+    var y = Option<Result<int, string>>.Some(Result<int, string>.Err("Bad mojo"));
+    var transpose = x.Transpose<int>();
+
+    Assert.True(y.Equals(transpose));
   }
 }
