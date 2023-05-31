4795db391fa20df27b4722881c1123e12743bdc6
Milo Wical <milo.wical@gmail.com>
2023-05-31 15:38:51 -0400

    Completed Option.GetOrInsert().

--- src/ResultCs/Option.cs
+++ src/ResultCs/Option.cs
@@ -9,10 +9,10 @@ public class Option<T>
   // ✓       ✓   ✓    T Expect(string message)
   // ✓       ✓   ✓    Option<T> Filter(Func<T, bool> predicate)
   // ✓       ✓   ✓    Option<T> Flatten()
-  //                  T GetOrInsert(T value)
+  // ✓       ✓   ✓    T GetOrInsert(T value)
   //                  T GetOrInsertWith(Func<T> f)
   //                  T Insert(T value)
-  //         ✓   ✓    bool IsNone()
+  // ✓       ✓   ✓    bool IsNone()
   // ✓       ✓   ✓    bool IsSome()
   //                  bool IsSomeAnd(Func<T, bool> f)
   //                  IEnumerable<Option<T>> Iter()
@@ -357,32 +357,39 @@ public class Option<T>
   }
 
   /// <summary>
-  /// Inserts <c>value<c> into the <c>Option</c> if it is <c>None</c>, then
-  /// returns a mutable reference to the contained value.
+  /// Inserts <c>value<c> into the <c>Option</c> if it is <c>None</c> and returns
+  /// the value stored.
   ///
-  /// See also <c>Option::insert</c>, which updates the value even if
+  /// See also <c>Option.Insert</c>, which updates the value even if
   /// the option already contains <c>Some</c>.
+  /// 
+  /// Note: This differs from the original implementation because it doesn't
+  /// return a mutable reference to the contained value due to differences in
+  /// how the languages work.
   ///
   /// Examples
   ///
   /// <code>
-  /// let mut x = None;
-  ///
-  /// {
-  ///     let y: &mut int = x.get_or_insert(5);
-  ///     assert_eq!(y, &5);
-  ///
-  ///     *y = 7;
-  /// }
-  ///
-  /// assert_eq!(x, Some(7));
+  /// var x = Option<int>.None();
+  /// var y = x.GetOrInsert(5);
+  /// 
+  /// Assert.Equal(5, y);
+  /// Assert.Equal(Option<int>.Some(5), x);
   /// <code>
   /// </summary>
-  /// <param name="value"></param>
-  /// <returns></returns>
+  /// <param name="value">The value to insert if <c>this</c> is <c>None</c>.</param>
+  /// <returns>The value contained in <c>this</c>.</returns>
   public T GetOrInsert(T value)
   {
-    throw new NotImplementedException();
+    ArgumentNullException.ThrowIfNull(value);
+
+    if(this.IsNone())
+    {
+      this._value = value;
+      this.Kind = OptionKind.Some;
+    }  
+
+    return _value!;
   }
 
   /// <summary>
@@ -445,14 +452,15 @@ public class Option<T>
   /// Examples
   ///
   /// <code>
-  /// let x: Option<int> = Some(2);
-  /// assert_eq!(x.is_none(), false);
+  /// var x = Option<int>.Some(2);
+  /// Assert.False(x.IsNone());
   ///
-  /// let x: Option<int> = None;
-  /// assert_eq!(x.is_none(), true);
+  /// var x = Option<int>.None();
+  /// Assert.True(x.IsNone());
   /// <code>
   /// </summary>
-  /// <returns></returns>
+  /// <returns><c>true</c> if <c>this</c> is <c>None</c>;
+  /// <c>false</c> otherwise.</returns>
   public bool IsNone() => Kind == OptionKind.None;
 
   /// <summary>
---
+++ test/ResultCs.Tests.Unit/Option/OptionGetOrInsertTests.cs
@@ -0,0 +1,31 @@
+namespace ResultCs.Tests.Unit.Option;
+
+public class OptionGetOrInsertTests
+{
+  [Fact]
+  public void OptionGetOrInsertNoneTest()
+  {
+    var x = Option<int>.None();
+    var y = x.GetOrInsert(5);
+
+    Assert.Equal(5, y);
+    Assert.Equal(Option<int>.Some(5), x);
+  }
+
+  [Fact]
+  public void OptionGetOrInsertSomeTest()
+  {
+    var x = Option<int>.Some(3);
+    var y = x.GetOrInsert(5);
+
+    Assert.Equal(3, y);
+    Assert.Equal(Option<int>.Some(3), x);
+  }
+
+  [Fact]
+  public void OptionGetOrInsertNullValueTest()
+  {
+    Assert.Throws<ArgumentNullException>(() => Option<string>.None().GetOrInsert(null!));
+    Assert.Throws<ArgumentNullException>(() => Option<string>.Some(string.Empty).GetOrInsert(null!));
+  }
+}
