a302e8fa2f6fb2f07018b616d8034cda17bc660f
Milo Wical <milo.wical@gmail.com>
2023-06-09 10:20:28 -0400

    Fixed OptionKind StyleCop comment errors.

--- src/ResultCs/OptionKind.cs
+++ src/ResultCs/OptionKind.cs
@@ -1,8 +1,19 @@
 // Copyright (c) Milo Wical. All rights reserved.
 
 namespace WicalWare.Components.ResultCs;
+
+/// <summary>
+/// An enum that helps identify if the <c>Option</c> has a value or not.
+/// </summary>
 public enum OptionKind
 {
+  /// <summary>
+  /// Indicates that the <c>Option</c> has a value.
+  /// </summary>
   Some,
+
+  /// <summary>
+  /// Indicates that the <c>Option</c> does not have a value.
+  /// </summary>
   None,
 }
