241e403fac71a5983a72c3fc910ae4692f708416
Milo Wical <milo.wical@gmail.com>
2023-06-09 10:33:07 -0400

    Completed ResultKind StyleCop comment errors.

--- src/ResultCs/ResultKind.cs
+++ src/ResultCs/ResultKind.cs
@@ -2,8 +2,18 @@
 
 namespace WicalWare.Components.ResultCs;
 
+/// <summary>
+/// An enum that helps identify if the <c>Result</c> contains a value or an error.
+/// </summary>
 public enum ResultKind
 {
+  /// <summary>
+  /// Indicates that the <c>Result</c> contains a value.
+  /// </summary>
   Ok,
+
+  /// <summary>
+  /// Indicates that the <c>Result</c> contains an error.
+  /// </summary>
   Err,
 }
