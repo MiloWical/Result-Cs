62a0e65bf391c29ff844ce3b18f7410195398c5b
Milo Wical <milo.wical@gmail.com>
2023-06-09 10:27:51 -0400

    Fixed PanicException StyleCop comment errors.

--- src/ResultCs/PanicException.cs
+++ src/ResultCs/PanicException.cs
@@ -2,6 +2,11 @@
 
 namespace WicalWare.Components.ResultCs;
 
+/// <summary>
+/// Represents an erroneous condition encountered in <c>Options</c>s and <c>Result</c>s.
+/// 
+/// It is intended to reflect the panic semantics in Rust.
+/// </summary>
 public class PanicException : Exception
 {
   private string message;
@@ -17,5 +22,10 @@ public class PanicException : Exception
     this.message = $"{errMessage}: {innerException.Message}";
   }
 
+  /// <summary>
+  /// Gets the panic message for the exception.
+  /// 
+  /// Overrides the base Object.Message property to return the intended panic message.
+  /// </summary>
   public override string Message => this.message;
 }
