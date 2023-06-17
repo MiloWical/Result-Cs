8017571cd0262bb093c01ab3686d08e319290479
Milo Wical <milo.wical@gmail.com>
2023-06-17 15:12:03 -0400

    Namespace scoping change.

--- test/ResultCs.Tests.Unit/Panic/PanicExceptionConstructorTests.cs
+++ test/ResultCs.Tests.Unit/Panic/PanicExceptionConstructorTests.cs
@@ -1,29 +1,28 @@
-namespace ResultCs.Tests.Unit.Panic
+namespace ResultCs.Tests.Unit.Panic;
+
+public class PanicExceptionConstructorTests
 {
-  public class PanicExceptionConstructorTests
+  [Fact]
+  public void PanicExceptionNullMessageNoInnerExceptionTest()
   {
-    [Fact]
-    public void PanicExceptionNullMessageNoInnerExceptionTest()
-    {
-      Assert.Throws<PanicException>(() => new PanicException(null!));
-    }
+    Assert.Throws<PanicException>(() => new PanicException(null!));
+  }
 
-    [Fact]
-    public void PanicExceptionNullMessageWithInnerExceptionTest()
-    {
-      Assert.Throws<PanicException>(() => new PanicException(null!, new Exception()));
-    }
+  [Fact]
+  public void PanicExceptionNullMessageWithInnerExceptionTest()
+  {
+    Assert.Throws<PanicException>(() => new PanicException(null!, new Exception()));
+  }
 
-    [Fact]
-    public void PanicExceptionMessageWithNullInnerExceptionTest()
-    {
-      Assert.Throws<PanicException>(() => new PanicException(string.Empty, null!));
-    }
+  [Fact]
+  public void PanicExceptionMessageWithNullInnerExceptionTest()
+  {
+    Assert.Throws<PanicException>(() => new PanicException(string.Empty, null!));
+  }
 
-    [Fact]
-    public void PanicExceptionNullMessageAndNullInnerExceptionTest()
-    {
-      Assert.Throws<PanicException>(() => new PanicException(null!, null!));
-    }
+  [Fact]
+  public void PanicExceptionNullMessageAndNullInnerExceptionTest()
+  {
+    Assert.Throws<PanicException>(() => new PanicException(null!, null!));
   }
 }
