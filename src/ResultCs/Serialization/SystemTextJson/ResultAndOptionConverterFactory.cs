393af3630281ea5794dbc94fc74b56740e836b1a
Milo Wical <milo.wical@gmail.com>
2024-01-06 09:47:11 -0500

    Added a System.Text.Json JsonConverterFactory.

---
+++ src/ResultCs/Serialization/SystemTextJson/ResultAndOptionConverterFactory.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Milo Wical. All rights reserved.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WicalWare.Components.ResultCs.Serialization.SystemTextJson;
+
+/// <summary>
+/// A factory that scaffolds the <see cref="OptionConverter{TSome}"/> and
+/// <see cref="ResultConverter{TOk, TErr}"/>.
+/// </summary>
+public class ResultAndOptionConverterFactory : JsonConverterFactory
+{
+  /// <inheritdoc />
+  public override bool CanConvert(Type typeToConvert)
+  {
+    if (!typeToConvert.IsGenericType)
+    {
+      return false;
+    }
+
+    var genericType = typeToConvert.GetGenericTypeDefinition();
+
+    return genericType == typeof(Result<,>) || genericType == typeof(Option<>);
+  }
+
+  /// <inheritdoc />
+  public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+  {
+    var baseType = typeToConvert.GetGenericTypeDefinition();
+
+    Type converterType;
+
+    if (baseType == typeof(Result<,>))
+    {
+      converterType = typeof(ResultConverter<,>);
+    }
+    else if (baseType == typeof(Option<>))
+    {
+      converterType = typeof(OptionConverter<>);
+    }
+    else
+    {
+      throw new ArgumentException($"Could not generate converter for generic type {baseType} of type {typeToConvert}.", nameof(typeToConvert));
+    }
+
+    var genericTypes = typeToConvert.GenericTypeArguments;
+
+    var converterInstanceType = converterType.MakeGenericType(genericTypes);
+    return Activator.CreateInstance(converterInstanceType) as JsonConverter;
+  }
+}
