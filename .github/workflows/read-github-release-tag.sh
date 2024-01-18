dcb4610ef465bd193ce5762933c9bfcb65d69409
Milo Wical <milo.wical@gmail.com>
2024-01-17 20:08:38 -0500

    Updated the version parsing regex.

--- .github/workflows/read-github-release-tag.sh
+++ .github/workflows/read-github-release-tag.sh
@@ -2,7 +2,7 @@
 
 GH_RELEASE=$(gh release list --repo "$1" --limit 1)
 RELEASE_TYPE=$(sed -rn 's/^\S+\s(\S+)\s.*/\1/p' <<< "$GH_RELEASE")
-CURRENT_RELEASE_TAG=$(sed -rn 's/(v[0-9]\.[0-9]\.[0-9](-\S+)?).*/\1/p' <<< "$GH_RELEASE")
+CURRENT_RELEASE_TAG=$(sed -rn 's/(v[0-9]+\.[0-9]+\.[0-9]+(-\S+)?).*/\1/p' <<< "$GH_RELEASE")
 
 if [ -z $CURRENT_RELEASE_TAG ]
 then
